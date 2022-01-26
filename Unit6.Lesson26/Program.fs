51a4a38dd924f2cdc8ce2a40c8579d2a5d2810ad
AlexanderTomashchuk <alexandertomashchuk@gmail.com>
2022-01-26 04:00:12 +0200

    unit 6 lesson 26

---
+++ Unit6.Lesson26/Program.fs
@@ -0,0 +1,6 @@
+open Unit6.Lesson26.Library
+
+[<EntryPoint>]
+let main argv =
+  let person = getPerson()
+  0
---
+++ Unit6.Lesson26/Scratchpad.fsx
@@ -0,0 +1,11 @@
+#r "nuget: Newtonsoft.Json"
+#r "nuget: Humanizer"
+
+#load "Library.fs"
+
+open Humanizer
+open Unit6.Lesson26
+
+"ScriptsAreAGreatWayToExplorePackages".Humanize()
+
+Library.getPerson()
